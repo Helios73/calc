b7e1def997c6ea08a8c7d2899f05c82034ae6a46
Helios73 <33083383+Helios73@users.noreply.github.com>
2019-09-05 21:00:53 +0400

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,38 +1,39 @@
 <?php
 
 if (isset($_POST[])) {
+    $var1 = $_POST['value1'];
+    $var1 = $_POST['value2'];
 
-	$var1 = $_POST['value1'];
-	$var1 = $_POST['value2'];
-
-	if ($_POST['operation'] == '+') {
-		$summa = $value1 + $value2 ;
-		echo $summa;
-		elseif ($_POST['opeation'] == '-') {
-		$summa = $value1 - $value2 ;
-		echo $summa;	
-		}
-		elseif ($_POST['operation'] == '/') {
-		$summa = $value1 / $value2 ;
-		echo $summa;
-		}
-		elseif ($_POST['operation'] == '*') {
-		$summa = $value1 * $value2 ;
-		echo $summa;
-		}
-	}
-
-
+    if ($_POST['operation'] == '+') {
+        $summa = $value1 + $value2 ;
+        echo $summa;
+    } elseif ($_POST['opeation'] == '-') {
+        $summa = $value1 - $value2 ;
+        echo $summa;	
+    } elseif ($_POST['operation'] == '/') {
+        $summa = $value1 / $value2 ;
+        echo $summa;
+    } elseif ($_POST['operation'] == '*') {
+        $summa = $value1 * $value2 ;
+        echo $summa;
+    }
 }
-
-<form action="cacl.php" method="post">
-
-	<input type="text" name="value1">
-
-	<select name="operation">
-		<option value="+">+</option>
-		<option value="-">-</option>
-		<option value="*">*</option>
-		<option value="/">/</option>
-	</select>
-    <input type="text" name "value2">
+?>
+<html>
+    <head></head>
+    <body>
+        <form action="calc.php" method="post">
+            <input type="text" name="value1">
+            <select name="operation">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="text" name="value2">
+            <input type="submit" name="submit" value="submit">
+            <input type="reset">
+        </form>
+    </body>
+</html>
+	
